4d492ab165cb5142f6476532af73c2bec7be212c
Lorenzo Torres <lorenzotorres@outlook.it>
2025-03-03 19:17:23 +0100

    Removed OpenGL renderer

--- src/render/opengl.zig
+++
@@ -1,8 +0,0 @@
-const c = @cImport({
-    @cDefine("GLAD_GL_IMPLEMENTATION", "1"); // Tells glad to implement the functions, this is standard in single header libraries (check out STB for a good example)
-    @cInclude("glad.h");
-});
-
-pub const Device = struct {
-    // device stuff
-};
